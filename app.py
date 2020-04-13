468564b24442a2a56ccfb5211262dc28925ccc9d
Eric <eric.l.hadley@protonmail.com>
2020-04-13 14:24:04 -0400

    added content

--- app.py
+++ app.py
@@ -14,7 +14,25 @@ inputs = numpy.column_stack((xs,zs))
 #generator list for noise
 noise = numpy.random.uniform(-1,1,(obs,1))
 
+# target element
 targets = 2*xs - 3*zs + 5 + noise
+
+init_range = 0.1
+
+weights = numpy.rand.uniform(-init_range,init_range,(2,1))
+biases = numpy.rand.uniform(-init_range,init_range,size=(1))
+
+learning_rate = 0.02
+
+'''
+Elements of the model in supervised learning
+1 -> inputs
+2 -> weights
+3 -> biases
+4 -> output
+5 -> targets
+'''
+
 
 targets = targets.reshape(obs,)
 fig = matplotlib.pyplot.figure()
